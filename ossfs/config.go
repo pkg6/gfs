c85aac63c7819f0389213c5d955c8390b2a9339c
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-03-14 23:55:22 +0800

    feat: config tag && tests

--- ossfs/config.go
+++ ossfs/config.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Config struct {
-	CDN    string
-	Bucket string
+	CDN    string `json:"cdn" xml:"CDN" yaml:"CDN"`
+	Bucket string `json:"bucket" xml:"Bucket" yaml:"Bucket"`
 	//https://help.aliyun.com/zh/oss/user-guide/regions-and-endpoints
-	Endpoint        string
-	AccessKeyID     string
-	AccessKeySecret string
-	OssConfig       *oss.Config
+	Endpoint        string      `json:"endpoint" xml:"Endpoint" yaml:"Endpoint"`
+	AccessKeyID     string      `json:"access_key_id" xml:"AccessKeyID" yaml:"AccessKeyID"`
+	AccessKeySecret string      `json:"access_key_secret" xml:"AccessKeySecret" yaml:"AccessKeySecret"`
+	Config          *oss.Config `json:"config" xml:"Config" yaml:"Config"`
 }
 
 func (c *Config) NewAdapter() gfs.IAdapter {
