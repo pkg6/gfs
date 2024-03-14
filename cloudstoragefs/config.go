573271494fc5c07c45caa0555c99f3978f50700f
chihqiang <zhiqiang2033@gmail.com>
2024-03-14 23:55:22 +0800

    feat: config tag && tests

--- cloudstoragefs/config.go
+++ cloudstoragefs/config.go
@@ -13,11 +13,11 @@ var (
 )
 
 type Config struct {
-	CDN             string
-	Bucket          string
-	WithTimeout     time.Duration
-	CredentialsFile string
-	CredentialsJSON string
+	CDN             string        `json:"cdn" xml:"CDN" yaml:"CDN"`
+	Bucket          string        `json:"bucket" xml:"Bucket" yaml:"Bucket"`
+	WithTimeout     time.Duration `json:"with_timeout" xml:"WithTimeout" yaml:"WithTimeout"`
+	CredentialsFile string        `json:"credentials_file" xml:"CredentialsFile" yaml:"CredentialsFile"`
+	CredentialsJSON string        `json:"credentials_json" xml:"CredentialsJSON" yaml:"CredentialsJSON"`
 	Option          []option.ClientOption
 }
 
