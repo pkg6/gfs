b653727c9ac3c563627267e3da458170cfb12dea
zzqqw <zhiqiangwang@aliyun.com>
2024-03-15 11:16:55 +0800

    Change method name

--- encipher.go
+++ encipher.go
@@ -2,16 +2,37 @@ package gfs
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"io"
 )
 
-func DecodeBase64(s string) ([]byte, error) {
+func Md5String(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Sha1String(str string) string {
+	h := sha1.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Sha256String(str string) string {
+	h := sha256.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Base64Decode(s string) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(s)
 }
 
-func EncodeBase64(src []byte) string {
+func Base64Encode(src []byte) string {
 	return base64.StdEncoding.EncodeToString(src)
 }
 
@@ -25,4 +46,16 @@ func Md5Reader(fr io.Reader) (string, error) {
 		return "", fmt.Errorf(`io.Copy failed`)
 	}
 	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+// Base64Reader
+//f, _ := os.Open("./_example/1.jpeg")
+//f.Seek(0, 0)
+//gfs.Base64Reader(f)
+func Base64Reader(fi io.Reader) (string, error) {
+	fd, err := io.ReadAll(fi)
+	if err != nil {
+		return "", err
+	}
+	return Base64Encode(fd), nil
 }
