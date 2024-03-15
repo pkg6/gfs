1369979c943602640a04a1973a4b54a3d77e6071
chihqiang <zhiqiang2033@gmail.com>
2024-03-15 11:06:51 +0800

    feat: Md5Reader

--- encipher.go
+++ encipher.go
@@ -1,6 +1,11 @@
 package gfs
 
-import "encoding/base64"
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
 
 func DecodeBase64(s string) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(s)
@@ -8,4 +13,16 @@ func DecodeBase64(s string) ([]byte, error) {
 
 func EncodeBase64(src []byte) string {
 	return base64.StdEncoding.EncodeToString(src)
+}
+
+// Md5Reader
+//f, _ := os.Open("./_example/1.jpeg")
+//f.Seek(0, 0)
+//gfs.Md5Reader(f)
+func Md5Reader(fr io.Reader) (string, error) {
+	h := md5.New()
+	if _, err := io.Copy(h, fr); err != nil {
+		return "", fmt.Errorf(`io.Copy failed`)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
